d1ebdf8bebc564877d46590274ed6e4071909855
Felos259 <43696085+Felos259@users.noreply.github.com>
2021-09-25 17:19:52 +0200

    GEGENER

--- Assets/_Workspaces/Workspace_User_FL/Scripts/Käfer.cs
+++ Assets/_Workspaces/Workspace_User_FL/Scripts/Käfer.cs
@@ -9,28 +9,42 @@ public class Käfer : MonoBehaviour
     [SerializeField] Sprite _deadSprite;
     bool _hasDied;
     [SerializeField] ParticleSystem _particleSystem;
+    public int speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //bewegt sich für die Zeit, die der letzte
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 
      void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if (ShouldDieFromCollosion(collision) == true)
         {
             Die();
         }
 
-        
+        if (ShouldTurnLeft(collision) == true)
+        {
 
+        }
+
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collision");
+        speed*= -1;
     }
 
     bool ShouldDieFromCollosion(Collision2D collision)
@@ -46,6 +60,27 @@ public class Käfer : MonoBehaviour
             return true;
         }
 
+        return false;
+    }
+
+    bool ShouldTurnLeft(Collision2D collision)
+    {
+        
+        TriggerLeft left = collision.gameObject.GetComponent<TriggerLeft>();
+        if (left != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool ShouldTurnRight(Collision2D collision)
+    {
+        TriggerRight right = collision.gameObject.GetComponent<TriggerRight>();
+        if (right != null)
+        {
+            return true;
+        }
         return false;
     }
 
